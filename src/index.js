ff86c331d96ea712188153c09ff663437cb68616
nisanadeem908 <nisa.nadeem@dubizzlelabs.com>
2024-04-17 11:50:37 +0500

    redux implementation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store'; 
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.getElementById('root');
+const app = (
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+if (root !== null && root instanceof HTMLElement) {
+  const rootInstance = ReactDOM.createRoot(root);
+  rootInstance.render(app);
+  
+  reportWebVitals(console.log);
+}
